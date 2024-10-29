98db567326d368f9eaf1049517d94bf57c8789c8
gegio82 <giorgio.gerosa@gmail.com>
2024-10-29 16:17:38 +0000

    Use defensive copy on Cart.getItems

--- src/main/java/it/ggerosa/shoppingcart/Cart.java
+++ src/main/java/it/ggerosa/shoppingcart/Cart.java
@@ -2,6 +2,7 @@ package it.ggerosa.shoppingcart;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -36,7 +37,7 @@ public class Cart {
     }
 
     public Set<Item> getItems() {
-        return items;
+        return Collections.unmodifiableSet(new HashSet<>(items));
     }
 
     public BigDecimal getTotalPrice() {
